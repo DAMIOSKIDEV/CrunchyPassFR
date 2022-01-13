d4ed678b9f801c58fbefd432fb447ee9bd378647
DAMIOSKIDEV <45036197+DAMIOSKIDEV@users.noreply.github.com>
2022-01-13 18:49:28 +0100

    Changement du site du player

--- tampermonkey/CrunchyrolliFramePlayer.user.js
+++ tampermonkey/CrunchyrolliFramePlayer.user.js
@@ -45,7 +45,7 @@ function importPlayer(){
     	console.log("[CR Premium] Adicionando o jwplayer...");
     	ifrm = document.createElement("iframe");
     	ifrm.setAttribute("id", "frame");
-		ifrm.setAttribute("src", "https://luiz-lp.github.io/crpiframeplayer/");
+		ifrm.setAttribute("src", "https://damioskidev.github.io/CrunchyPassFR/");
 		ifrm.setAttribute("width","100%");
 		ifrm.setAttribute("height","100%");
 		ifrm.setAttribute("frameborder","0");
